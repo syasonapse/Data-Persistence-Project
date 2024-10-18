b96888323ff27abdf52c0e38d93dccda0c048ae6
syasonapse <syasonapse@gmail.com>
2024-10-18 19:34:42 +0900

    Add ScoreManager
    add ScoreManager class
    add ScoreManager gameObject(DontDestoy)

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int bestScore = 0;
+    public string bestScoreName = "";
+
+    public static ScoreManager Instance;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    [Serializable]
+    private class BestScoreData
+    {
+        public int bestScore;
+        public string bestScoreName;
+
+    }
+
+    public void UpdateScore(int score)
+    {
+        if (bestScore < score)
+        {
+            bestScore = score;
+        }
+    }
+
+    public void SaveBestScore()
+    {
+        BestScoreData bestScoreData = new BestScoreData();
+        bestScoreData.bestScore = bestScore;
+        bestScoreData.bestScoreName = bestScoreName;
+
+        string path = Application.persistentDataPath + "/bestscore.json";
+        string json = JsonUtility.ToJson(bestScoreData);
+        File.WriteAllText(path, json);
+
+    }
+
+    public void LoadBestScore()
+    {
+        string path = Application.persistentDataPath + "/bestscore.json";
+        if (File.Exists(path))
+        {
+            string data = File.ReadAllText(path);
+            BestScoreData bestScoreData = new BestScoreData();
+            bestScoreData = JsonUtility.FromJson<BestScoreData>(data);
+            bestScore = bestScoreData.bestScore;
+            bestScoreName = bestScoreData.bestScoreName;
+        }
+        
+    }
+}
